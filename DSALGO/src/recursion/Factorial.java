915674d1df2d8020b0177b4a6f02f6387b0d3117
madhavSharma23 <74015807+madhavSharma23@users.noreply.github.com>
2022-05-27 07:41:35 +0530

    Modified

--- DSALGO/src/recursion/Factorial.java
+++ DSALGO/src/recursion/Factorial.java
@@ -1,6 +1,12 @@
 package recursion;
 
 public class Factorial {
+    /*
+    Factorial of a number is the product of 1 to that number
+    i.e 4! = 4*3*2*1 = 24
+    3! = 3*2*1 = 7
+     */
+
     public static int factorial_recursive(int n){
         //additional condition check
         if(n<=0){
